2d278fe4a7ee10a0b6e828b3d9a903856155e947
Jan Pospisil <me@jan-pospisil.cz>
2019-01-22 11:42:35 +0100

    Added arguments to Controller extension

--- src/ControllersExtension.php
+++ src/ControllersExtension.php
@@ -7,7 +7,8 @@ declare(strict_types = 1);
 
 namespace RestServer;
 use Nette\DI\CompilerExtension;
-use Nette\Utils\Strings;
+use Nette\DI\ServiceDefinition;
+use Nette\Utils\Strings;;
 
 /**
  * ControllersExtensions
@@ -16,15 +17,33 @@ use Nette\Utils\Strings;
 
 class ControllersExtension extends CompilerExtension {
 
+	/**
+	 * @throws \ReflectionException
+	 */
 	public function loadConfiguration(): void {
+		// TODO: setup
 		$builder = $this->getContainerBuilder();
 		$config = $this->getConfig();
-		foreach($config as $name => $className){
-			$r = new \ReflectionClass($className);
-			if(!$r->implementsInterface('RestServer\IController')){
-				throw new \InvalidArgumentException('Class "'.$className.'" must implement interface "RestServer\IController"');
+		foreach($config as $name => $classDefinition){
+			if(is_scalar($classDefinition)){
+				$className = $classDefinition;
+				$r = new \ReflectionClass($className);
+				if(!$r->implementsInterface('RestServer\IController')){
+					throw new \InvalidArgumentException('Class "'.$className.'" must implement interface "RestServer\IController"');
+				}
+				$builder->addDefinition(strtr(Strings::webalize($className), array('-' => '_')))->setType($className);
+			} else {
+				$className = $classDefinition['class'];
+				$r = new \ReflectionClass($className);
+				if(!$r->implementsInterface('RestServer\IController')){
+					throw new \InvalidArgumentException('Class "'.$className.'" must implement interface "RestServer\IController"');
+				}
+				$name = strtr(Strings::webalize($className), array('-' => '_'));
+				$def = new ServiceDefinition();
+				$def->setType($classDefinition['class']);
+				$def->setArguments($classDefinition['arguments']);
+				$builder->addDefinition($name, $def);
 			}
-			$builder->addDefinition(strtr(Strings::webalize($className), array('-' => '_')))->setClass($className);
 		}
 	}
 
